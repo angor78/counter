7b8cf4723d279383403970c97f60586a915a28b7
Angor78 <angor78@gmail.com>
2022-06-20 10:49:36 +0300

    nest App to Provider Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,85 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './App.css';
-import {Display} from "./components/Display/Display";
-import {Button} from "./components/Button/Button";
-import {SetCounterDisplay} from "./components/SetCounterDisplay/SetCounterDisplay";
+import {useDispatch, useSelector} from "react-redux";
+import {AppStateType} from "./bll/store";
+import {incValueAC} from "./bll/counter-reducer";
 
 
-export type ButtonPropsType = {
-  setCountCallback: () => void
-  isDisabled: boolean
-  name: string
-}
-export type CounterType = {
-  start: number
-  current: number
-  target: number
-}
+
+// export type ButtonPropsType = {
+//   setCountCallback: () => void
+//   isDisabled: boolean
+//   name: string
+// }
 
 function App() {
 
-  const [counter, setCounter] = useState<CounterType>({start: 0, current: 0, target: 5})
-  const [settingMode, setSettingMode] = useState<boolean>(false)
-  useEffect(() => {
-    let startValueString = localStorage.getItem('startValue')
-    let targetValueString = localStorage.getItem('targetValue')
-    if (startValueString && targetValueString) {
-      let startNumber = JSON.parse(startValueString)
-      let targetNumber = JSON.parse(targetValueString)
-      setCounter({
-        ...counter,
-        start: startNumber,
-        current: startNumber,
-        target: targetNumber
-      })
-    }
-
-  }, [])
-
-  let isPositive = counter.start >= 0 && counter.target >= 0
-  let isStartLessTarget = counter.start < counter.target
+  const value = useSelector<AppStateType,number>(state => state.counter.start)
+  const dispatch = useDispatch()
 
   const setCountCallbackHandler = () => {
-    if (counter.current < counter.target) {
-      setCounter({...counter, current: counter.current + 1})
-    }
-  }
-  const resetCallbackHandler = () => {
-    setCounter({...counter, current: counter.start})
+    dispatch(incValueAC())
+    // if (counter.current < counter.target) {
+    //   setCounter({...counter, current: counter.current + 1})
+    // }
   }
 
-  function setStart(startValue: number) {
-    localStorage.setItem("startValue", JSON.stringify(startValue))
-    setCounter({...counter, start: startValue})
-  }
+  // const [counter, setCounter] = useState<CounterType>({start: 0, current: 0, target: 5})
+  // const [settingMode, setSettingMode] = useState<boolean>(false)
+  // useEffect(() => {
+  //   let startValueString = localStorage.getItem('startValue')
+  //   let targetValueString = localStorage.getItem('targetValue')
+  //   if (startValueString && targetValueString) {
+  //     let startNumber = JSON.parse(startValueString)
+  //     let targetNumber = JSON.parse(targetValueString)
+  //     setCounter({
+  //       ...counter,
+  //       start: startNumber,
+  //       current: startNumber,
+  //       target: targetNumber
+  //     })
+  //   }
+  //
+  // }, [])
+  //
+  // let isPositive = counter.start >= 0 && counter.target >= 0
+  // let isStartLessTarget = counter.start < counter.target
+  //
+  // const setCountCallbackHandler = () => {
+  //   if (counter.current < counter.target) {
+  //     setCounter({...counter, current: counter.current + 1})
+  //   }
+  // }
+  // const resetCallbackHandler = () => {
+  //   setCounter({...counter, current: counter.start})
+  // }
+  //
+  // function setStart(startValue: number) {
+  //   localStorage.setItem("startValue", JSON.stringify(startValue))
+  //   setCounter({...counter, start: startValue})
+  // }
+  //
+  // function setTarget(targetValue: number) {
+  //   localStorage.setItem("targetValue", JSON.stringify(targetValue))
+  //   setCounter({...counter, target: targetValue})
+  // }
 
-  function setTarget(targetValue: number) {
-    localStorage.setItem("targetValue", JSON.stringify(targetValue))
-    setCounter({...counter, target: targetValue})
-  }
 
-
-  function getFromLocalStorage() {
-    let startValueString = localStorage.getItem('startValue')
-    let targetValueString = localStorage.getItem('targetValue')
-    if (startValueString && targetValueString) {
-      let startNumber = JSON.parse(startValueString)
-      let targetNumber = JSON.parse(targetValueString)
-      setCounter({
-        ...counter,
-        start: startNumber,
-        current: startNumber,
-        target: targetNumber
-      })
-    }
-    setSettingMode(false)
-  }
+  // function getFromLocalStorage() {
+  //   let startValueString = localStorage.getItem('startValue')
+  //   let targetValueString = localStorage.getItem('targetValue')
+  //   if (startValueString && targetValueString) {
+  //     let startNumber = JSON.parse(startValueString)
+  //     let targetNumber = JSON.parse(targetValueString)
+  //     setCounter({
+  //       ...counter,
+  //       start: startNumber,
+  //       current: startNumber,
+  //       target: targetNumber
+  //     })
+  //   }
+  //   setSettingMode(false)
+  // }
 
   // function clearLocalStorage() {
   //   localStorage.clear()
@@ -82,42 +88,41 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <div className='counterBlock'>
-          <SetCounterDisplay setStart={setStart} setTarget={setTarget} counter={counter}/>
-          <Button name={"set"} setCountCallback={getFromLocalStorage} isDisabled={!(isStartLessTarget && isPositive)}/>
-          {/*<Button name={"clear"} setCountCallback={clearLocalStorage} isDisabled={false}/>*/}
-        </div>
-        <div className='counterBlock'>
-          <Display counter={counter}/>
-          <Button name={"inc..+"} setCountCallback={setCountCallbackHandler}
-                  isDisabled={counter.current >= counter.target}/>
-          <Button name={"reset"} setCountCallback={resetCallbackHandler} isDisabled={false}/>
-        </div>
-      </div>
-      <div>
-        {settingMode
-          ? (
-          <div className='counterBlock'>
-            <SetCounterDisplay setStart={setStart} setTarget={setTarget} counter={counter}/>
-            <Button name={"set"} setCountCallback={getFromLocalStorage}
-                    isDisabled={!(isStartLessTarget && isPositive)}/>
-            {/*<Button name={"clear"} setCountCallback={clearLocalStorage} isDisabled={false}/>*/}
-          </div>)
-          : (
-          <div className='counterBlock'>
-            <Display counter={counter}/>
-            <Button name={"inc..+"} setCountCallback={setCountCallbackHandler}
-                    isDisabled={counter.current >= counter.target}/>
-            <Button name={"reset"} setCountCallback={resetCallbackHandler} isDisabled={false}/>
-            <Button name={"set"} setCountCallback={()=>setSettingMode(true)} isDisabled={false}/>
-          </div>
-        )
-        }
-
-
-      </div>
-
+      {value}
+      <button onClick={setCountCallbackHandler}>inc+...</button>
+      {/*<div>*/}
+      {/*  <div className='counterBlock'>*/}
+      {/*    <SetCounterDisplay setStart={setStart} setTarget={setTarget} counter={counter}/>*/}
+      {/*    <Button name={"set"} setCountCallback={getFromLocalStorage} isDisabled={!(isStartLessTarget && isPositive)}/>*/}
+      {/*    /!*<Button name={"clear"} setCountCallback={clearLocalStorage} isDisabled={false}/>*!/*/}
+      {/*  </div>*/}
+      {/*  <div className='counterBlock'>*/}
+      {/*    <Display counter={counter}/>*/}
+      {/*    <Button name={"inc..+"} setCountCallback={setCountCallbackHandler}*/}
+      {/*            isDisabled={counter.current >= counter.target}/>*/}
+      {/*    <Button name={"reset"} setCountCallback={resetCallbackHandler} isDisabled={false}/>*/}
+      {/*  </div>*/}
+      {/*</div>*/}
+      {/*<div>*/}
+      {/*  {settingMode*/}
+      {/*    ? (*/}
+      {/*    <div className='counterBlock'>*/}
+      {/*      <SetCounterDisplay setStart={setStart} setTarget={setTarget} counter={counter}/>*/}
+      {/*      <Button name={"set"} setCountCallback={getFromLocalStorage}*/}
+      {/*              isDisabled={!(isStartLessTarget && isPositive)}/>*/}
+      {/*      /!*<Button name={"clear"} setCountCallback={clearLocalStorage} isDisabled={false}/>*!/*/}
+      {/*    </div>)*/}
+      {/*    : (*/}
+      {/*    <div className='counterBlock'>*/}
+      {/*      <Display counter={counter}/>*/}
+      {/*      <Button name={"inc..+"} setCountCallback={setCountCallbackHandler}*/}
+      {/*              isDisabled={counter.current >= counter.target}/>*/}
+      {/*      <Button name={"reset"} setCountCallback={resetCallbackHandler} isDisabled={false}/>*/}
+      {/*      <Button name={"set"} setCountCallback={()=>setSettingMode(true)} isDisabled={false}/>*/}
+      {/*    </div>*/}
+      {/*  )*/}
+      {/*  }*/}
+      {/*</div>*/}
     </div>
   );
 }
